2cd4be1d3c771887715337e471518a39c14c340f
Baylor Whitehead <38015563+BaylorWhitehead@users.noreply.github.com>
2019-01-28 19:20:55 -0800

    implimented command lines args

--- batteryTest.py
+++ batteryTest.py
@@ -13,7 +13,7 @@ import time
 import datetime
 import os  
 from os.path import join as pjoin
-
+import sys
 
 #Sets up connections to all devices. 
 #Note the open resource input needs to be changed based to correct input,
@@ -23,11 +23,10 @@ def setup():
     print(rm.list_resources())
     
     eLoad = rm.open_resource('GPIB0::1::INSTR')
-    mMeter1 = rm.open_resource('USB0::0x1AB1::0x09C4::DM3R193601853::INSTR')
-    mMeter2 = rm.open_resource('USB0::0x1AB1::0x09C4::DM3R191700643::INSTR')
-    pSupply = rm.open_resource('USB0::0x1AB1::0x0E11::DP8C192803275::INSTR')
-    
-    directory = "lol"
+    mMeter1 = rm.open_resource('USB0::0x1AB1::0x09C4::DM3R193601828::INSTR')
+    mMeter2 = rm.open_resource('USB0::0x1AB1::0x09C4::DM3R193601838::INSTR')
+    pSupply = rm.open_resource('USB0::0x1AB1::0x0E11::DP8C192803287::INSTR')
+    directory = "{}_Data".format(sys.argv[1])
     
     try:
         if not os.path.exists(directory):
@@ -36,49 +35,52 @@ def setup():
         print ('Error: Creating directory. ' +  directory)
     return eLoad, mMeter1, mMeter2, pSupply
 def main():
-    try:
-        eLoad, mMeter1, mMeter2, pSupply = setup()
-        cycle = 0
+    eLoad, mMeter1, mMeter2, pSupply = setup()
+    #try:
         
-        while(cycle < 1):
-            pSupplyOff(pSupply)
-            eLoadOff(eLoad)
-            time.sleep(1)
-            v = mMeterGetV(mMeter1)
-            print(v)
-            if(float(v) < 3.67):  #if charge low then charge
-                filepath =  "C:\\Users\\bwhitehe\\Desktop\\BatteryCharacterizerPyVISA-master\\lol\\batC{0}.csv".format(cycle+1)
-                f = open(filepath, "w")
-                f.write("Time,Volt,Current\n")
-                pSupplySetup(pSupply)
-                start_time = time.time()
-                while(float(v) < 3.8): #charges until Volts hit max
-                    time.sleep(1.0 - ((time.time() - start_time) % 1.0))
-                    v, i = charge(mMeter1, mMeter2)
-                    time_now = time.time()-start_time
-                    f.write(("{0:d},{1:.4f},{2:.4f}\n").format(int(time_now), float(v), float(i)))
-                    
-                pSupplyOff(pSupply)
-                f.close()
-            else:                #else if votls high discharge
-                filepath =  "C:\\Users\\bwhitehe\\Desktop\\BatteryCharacterizerPyVISA-master\\lol\\batD{0}.csv".format(cycle+1)
-                f = open(filepath, "w")
-                f.write("Time,Volt,Current\n")
-                eLoadSetup(eLoad);
-                start_time = time.time()
-                while(float(v) >= 3.5): #discharges until Volts hit low
-                    print("helllll")
-                    time.sleep(1.0 - ((time.time() - start_time) % 1.0))
-                    v,i = discharge(mMeter1, mMeter2, eLoad)
-                    time_now = time.time()-start_time
-                    f.write(("{0:d},{1:.4f},{2:.4f}\n").format(int(time_now), float(v), float(i)))
-                eLoadOff(eLoad)
-                cycle += 1
-                f.close()
-    except:
-        print("failed")
-        eLoadOff(eLoad)
+    cycle = 0
+    
+    while(cycle < 1):
         pSupplyOff(pSupply)
+        eLoadOff(eLoad)
+        print("blaa")
+        time.sleep(1)
+        v = mMeterGetV(mMeter1)
+        print(v)
+        if(float(v) < 3.67):  #if charge low then charge
+            filepath =  "C:\\Users\\bwhitehe\\Desktop\\BatteryCharacterizerPyVISA-master\\{0}_Data\\batC{1}.csv".format(sys.argv[1],cycle+1)
+            f = open(filepath, "w")
+            print("here")
+            f.write("Time,Volt,Current\n")
+            pSupplySetup(pSupply)
+            start_time = time.time()
+            while(float(v) < 3.8): #charges until Volts hit max
+                time.sleep(1.0 - ((time.time() - start_time) % 1.0))
+                v, i = charge(mMeter1, mMeter2)
+                time_now = time.time()-start_time
+                f.write(("{0:d},{1:.4f},{2:.4f}\n").format(int(time_now), float(v), float(i)))
+                
+            pSupplyOff(pSupply)
+            f.close()
+        else:                #else if votls high discharge
+            filepath =  "C:\\Users\\bwhitehe\\Desktop\\BatteryCharacterizerPyVISA-master\\{0}_Data\\batD{1}.csv".format(sys.argv[1],cycle+1)
+            f = open(filepath, "w")
+            f.write("Time,Volt,Current\n")
+            eLoadSetup(eLoad);
+            start_time = time.time()
+            while(float(v) >= 3.5): #discharges until Volts hit low
+                print("helllll")
+                time.sleep(1.0 - ((time.time() - start_time) % 1.0))
+                v,i = discharge(mMeter1, mMeter2, eLoad)
+                time_now = time.time()-start_time
+                f.write(("{0:d},{1:.4f},{2:.4f}\n").format(int(time_now), float(v), float(i)))
+            eLoadOff(eLoad)
+            cycle += 1
+            f.close()
+    #except:
+        #print("failed")
+        #eLoadOff(eLoad)
+        #pSupplyOff(pSupply)
         
 
 def eLoadSetup(eLoad):
